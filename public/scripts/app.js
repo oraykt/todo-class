f8d5efa91bb96700949f7bf42dea42a902535ac0
OrayKT <oraykt@gmail.com>
2021-07-04 13:39:42 +0200

    initial setup

---
+++ public/scripts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var root = document.getElementById('root');
+var template = /*#__PURE__*/React.createElement("h1", null, "Hello World"); // ReactDOM
+
+ReactDOM.render(template, root);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+const root = document.getElementById('root');
+
+const template = <h1>Hello World</h1>
+
+// ReactDOM
+ReactDOM.render(template, root);
